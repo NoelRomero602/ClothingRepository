4dd663a24df4af3c39b44de06a36a022a7958064
mask <Jarvasjdk>
2020-07-08 16:10:50 -0500

    fixed "Access denied for user springstudent@localhost"

--- BackEnd/ClothingTable.sql
+++ BackEnd/ClothingTable.sql
@@ -1,3 +1,11 @@
+
+ DROP USER if exists 'springstudent'@'localhost' ;
+ 
+ -- Now create user with proper privs
+ CREATE USER 'springstudent'@'localhost' IDENTIFIED BY 'springstudent';
+ GRANT ALL PRIVILEGES ON * . * TO 'springstudent'@'localhost';
+ ALTER USER 'springstudent'@'localhost' IDENTIFIED WITH mysql_native_password BY 'springstudent';
+
 CREATE DATABASE  IF NOT EXISTS `Clothing_Article_directory`;
 USE `Clothing_Article_directory`;
 
