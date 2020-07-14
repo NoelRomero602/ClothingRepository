73199267808932ee5a0b734773b750f49a5b7e58
mask <Jarvasjdk>
2020-07-14 02:26:52 -0500

    added angular?

--- FrontEnd/angular-clothing/karma.conf.js
+++ FrontEnd/angular-clothing/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/angular-clothing'),
+      dir: require('path').join(__dirname, './coverage/Angular-Clothing'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- FrontEnd/angular-clothing/src/app/app.component.spec.ts
+++ FrontEnd/angular-clothing/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-clothing'`, () => {
+  it(`should have as title 'Angular-Clothing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-clothing');
+    expect(app.title).toEqual('Angular-Clothing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-clothing app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Angular-Clothing app is running!');
   });
 });
